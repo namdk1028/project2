69b5da971428f5fda7365adf1df36c181313218e
kimnayu <mung0605@naver.com>
2020-09-09 15:32:51 +0900

    [B] add: register accounts app in settings

--- back/back/settings.py
+++ back/back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sg)o^k1*ms6$i9%i91pk+o142qm@0ihtntr%7wb%7vxkjssnp='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
