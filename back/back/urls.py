2a31b25876c191b10b63a41bf0338167426bb67a
kimnayu <mung0605@naver.com>
2020-09-17 16:43:32 +0900

    [B] fix: typo in back/urls.py

--- back/back/urls.py
+++ back/back/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('interview/', include('interviews.urls')),
+]
